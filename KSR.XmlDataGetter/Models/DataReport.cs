7c2f352f69ae7aeeeae6ca8ed084567d02c78392
210137 Arkuszyński Jan <210137@edu.p.lodz.pl>
2019-04-02 13:58:38 +0200

    initial commit

---
+++ KSR.XmlDataGetter/Models/DataReport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KSR.XmlDataGetter.Models
+{
+    public class DataReport
+    {
+        public string Title { get; set; }
+        public string Dateline { get; set; }
+        public string Body { get; set; }
+
+        public DataReport(string title, string dateline, string body)
+        {
+            
+        }
+    }
+}
