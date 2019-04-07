a2fe5e26a9f2d705a30549da28bedc9f6324c7d3
Maciej Majchrowski <maciek21197@gmail.com>
2019-04-08 00:34:49 +0200

    added euclidean metric and  preprering knn clasification

---
+++ Classificator/KNNClassificator.cs
@@ -0,0 +1,42 @@
+﻿using KSR.Metrics;
+using KSR.XmlDataGetter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classificator
+{
+    public class KNNClassificator
+    {
+        /*
+         1. get traing data, k, maximal distance
+         2. get testing data
+         3. for each element in testing data count distance to each training data - sort it ascendly 
+         4. take label from the nearest neighbous and writes it to testing data entity
+         */
+        private List<DataFeatureDictionary> trainingData = new List<DataFeatureDictionary>();
+        private int k;
+        private int maxDistance;
+        private IMetric metric;
+
+        public KNNClassificator(List<DataFeatureDictionary> trainingData,int k,int maxDistance,IMetric metric)
+        {
+            this.trainingData = trainingData;
+            this.k = k;
+            this.maxDistance = maxDistance;
+            this.metric = metric;
+        }
+
+        public List<DataFeatureDictionary> classify(DataFeatureDictionary testingData)
+        {
+            List<DataFeatureDictionary> classifiedData = new List<DataFeatureDictionary>();
+
+
+            return classifiedData;
+        }
+
+
+    }
+}
