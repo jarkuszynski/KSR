eb17ac4264733558fbe8f549c67f1f21fd09376b
Maciej Majchrowski <maciek21197@gmail.com>
2019-04-15 01:56:38 +0200

    fix

--- KSR.Extractors/MostCommonKeyWordsExtractor.cs
+++ KSR.Extractors/MostCommonKeyWordsExtractor.cs
@@ -86,7 +86,7 @@ namespace KSR.Extractors
             }
             var countedAllWords = allWordsCouted.OrderByDescending(d => d.Value).ToDictionary(k => k.Key, v => v.Value);
             List<string> mostCommonWords = new List<string>();
-            for (int k = 0; k < 20; k++)
+            for (int k = 0; k < NUMBER_OF_COMMON_WORDS; k++)
             {
                 mostCommonWords.Add(countedAllWords.ElementAt(k).Key);
             }
